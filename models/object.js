6cff4dff7d980b45b286d72a533ae8a5a8a61040
caitreid <ctlnrd@gmail.com>
2023-01-17 20:20:36 -0500

    packages, model, schema

---
+++ models/object.js
@@ -0,0 +1,45 @@
+// import mongoose
+const mongoose = require('mongoose')
+
+// destructure Schema and model
+const { Schema, model } = mongoose
+
+// Object schema
+const objectSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    period: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    culture: {
+        type: String,
+        required: true
+    },
+    medium: {
+        type: String,
+        required: true
+    },
+    classification: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: false
+    }
+})
+
+const Object = model('Object', objectSchema)
+
+module.exports = Object
---
+++ server.js
@@ -0,0 +1,8 @@
+// dependencies
+const express = require('express') 
+const mongoose = require('mongoose') 
+const morgan = require('morgan') 
+require('dotenv').config() 
+const path = require('path') 
+
+// import model
